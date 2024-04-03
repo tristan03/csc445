fa44aad9bdfa611d24ea20bca1bae539deada954
tristan03 <133801805+tristan03@users.noreply.github.com>
2024-04-03 17:40:30 -0400

    download now works TCP sliding window
    upload already worked prior to this. download now works too. almost finished. added a bunch of comments as well.

--- csc445/Assignment2/Main.java
+++ csc445/Assignment2/Main.java
@@ -10,11 +10,19 @@ import java.util.*;
 
 public class Main {
 
+    /*
+        javac -d bin src/Assignment2/XorEncryption.java src/Assignment2/Main2.java src/Assignment2/Server.java
+        src/Assignment2/BytesHandler.java src/Assignment2/FileHandler.java
+
+        java -cp bin Assignment2.Main
+        java -cp bin Assignment2.Server
+     */
+
     static int port = 3030;
     static String host = "129.3.20.3";  // moxie's ip
     static final int WINDOW_SIZE = 4;
 
-    static Map<Integer, Boolean> ackMap = new HashMap<>();
+    static Map<Integer, Boolean> ackMap = new HashMap<>();  // map to keep track of what packets have been acknowledged
 
     public static void main(String[] args) {
         System.out.print("Upload/Download: ");
@@ -35,25 +43,39 @@ public class Main {
         Scanner fileScanner = new Scanner(System.in);
         String filename = fileScanner.next();
 
+        // TODO: remove and take a command line arg
+        System.out.print("Drop? ");
+        Scanner dropScanner = new Scanner(System.in);
+        String dropInput = dropScanner.next();
+        if (dropInput.equalsIgnoreCase("y")) {
+            drop = true;
+        }
+
         if (input.equalsIgnoreCase("upload")) {
-            File file = FileHandler.getFile(filename);
+            File file = FileHandler.getFile(filename);  // get desired file to upload
             if (file != null) {
-                sendWithTCP(randomNumber, senderID, drop, file);
+                sendWithTCP(randomNumber, senderID, drop, file);    // upload file
             } else {
                 System.err.println(filename + " cannot be found. ");
             }
         } else if (input.equalsIgnoreCase("download")) {
-            File file = downloadWithTCP(filename);
+            File file = downloadWithTCP(filename);  // download desired file
 
-            File currentFile = FileHandler.getFile(filename);
+            File currentFile = null;
+            try {
+                currentFile = FileHandler.getFile(filename);   // get old file
+            } catch (Exception e) {
+                System.err.println("Cannot validate files. " + filename + " was not previously present on your machine. ");
+            }
 
-            assert currentFile != null;
-            String currentFileContents = FileHandler.readFile(currentFile);
-            String newFileContents = FileHandler.readFile(file);
+            if (currentFile != null) {
+                String currentFileContents = FileHandler.readFile(currentFile); // get old file contents
+                String newFileContents = FileHandler.readFile(file);    // get new file contents
 
-            assert currentFileContents != null;
-            if (currentFileContents.equals(newFileContents)) {
-                System.out.println("Valid file contents. ");
+                assert currentFileContents != null;
+                if (currentFileContents.equals(newFileContents)) {  // validate
+                    System.out.println("Valid file contents. ");
+                }
             }
         }
     }
@@ -67,6 +89,7 @@ public class Main {
         byte[] data = BytesHandler.stringToBytes(fileContents);
         byte[] message = xorEncryption.encrypt(data, key);
 
+        // fill ack map with every packet not yet acknowledged
         for (int i = 0; i < message.length; i++) {
             ackMap.put(i, false);
         }
@@ -75,18 +98,20 @@ public class Main {
             ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream())) {
 
-            outputStream.writeInt(0);
-            outputStream.writeObject(senderID);
-            outputStream.writeObject(randomNumber);
-            outputStream.writeInt(message.length);
+            outputStream.writeInt(0);   // send mode (upload)
+            outputStream.writeObject(senderID); // send sender ID
+            outputStream.writeObject(randomNumber);  // send long for decryption
+            outputStream.writeInt(message.length);  // send message length
 
+            // initialize variables
             int totalPackets = message.length;
             System.out.println("Total packets: " + totalPackets);
             int ackReceived = 0;
             int startPacket = -1;
 
+            // sending file
             do {
-
+                // fill initial window
                 if (startPacket == -1) {
                     for (int i = 0; i < WINDOW_SIZE; i++) {
                         byte packet = message[i];
@@ -95,12 +120,14 @@ public class Main {
                     }
                 }
 
+                // send the rest upon receiving expecting ack of packetToSend - WINDOW_SIZE
                 try {
                     Object ack = inputStream.readObject();
 
+                    // receive ack
                     if (ack instanceof Integer) {
                         ackReceived = (Integer) ack;
-                        updateAckMap(ackReceived);
+                        updateAckMap(ackReceived);  // update the ack map (packet was acknowledged)
                     }
 
                     int remainingPackets = message.length - ackReceived;
@@ -111,10 +138,15 @@ public class Main {
                         if (remainingPackets > WINDOW_SIZE) {
                             startPacket = ackReceived + WINDOW_SIZE;  // slide window
                             System.out.println("Window slid, sending packet " + startPacket);
-
                             if (startPacket < message.length && packetWasReceived(startPacket)) {
                                 byte packet = message[startPacket];
-                                outputStream.writeObject(packet);
+
+                                // if the drop flag was entered, drop 1% of packets
+                                if (drop && shouldDropPacket()) {
+                                    System.out.println("Dropped packet " + startPacket);
+                                } else {    // upload normally, no dropped packets
+                                    outputStream.writeObject(packet);
+                                }
                             }
                         }
                     }
@@ -124,6 +156,12 @@ public class Main {
                 }
 
             } while (ackReceived != message.length - 1);
+
+            // flush and close streams
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
             System.out.println("All packets acknowledged. (" + ackReceived + " packets)");
 
         } catch (IOException e) {
@@ -135,14 +173,64 @@ public class Main {
         try (Socket socket = new Socket(host, port);
              ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream())) {
             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
-            outputStream.writeInt(1);
-            outputStream.writeObject(filename);
 
-            File file = (File) inputStream.readObject();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            Object packet;
+            int packetCount = 0;
+            int packetToAck = 0;
 
-            FileHandler.saveFile(file);
+            outputStream.writeInt(1);   // send mode (download)
+            outputStream.writeObject(filename); // send filename
 
-            return file;
+            String senderID = (String) inputStream.readObject();    // read senderID
+            int messageLength = inputStream.readInt();  // read message length
+            long randomNumber = inputStream.readLong();
+
+            do {
+                if (packetToAck == 0) {
+                    for (int i = 0; i < WINDOW_SIZE; i++) {
+                        packet = inputStream.readObject();
+                        baos.write((byte) packet);
+
+                        System.out.println("Received packet " + packetCount);
+                        packetCount++;
+                    }
+                } else if (packetCount != messageLength) {
+                    packet = inputStream.readObject();
+                    baos.write((byte) packet );
+                    System.out.println("Received packet " + packetCount);
+                    packetCount++;
+                }
+
+                // send ack
+                outputStream.writeObject(packetToAck);
+                System.out.println("Ack sent for packet " + packetToAck);
+
+                packetToAck++;
+
+            } while (packetToAck != messageLength);
+
+            byte[] message = baos.toByteArray();
+
+            String filename1 = senderID + ".txt";
+
+            System.out.println(filename1);
+            System.out.println(randomNumber);
+
+            long key = XorShift(randomNumber);
+            XorEncryption xorEncryption = new XorEncryption(key);
+            message = xorEncryption.decrypt(message, key);
+            String contents = BytesHandler.bytesToString(message);
+
+            System.out.println("File contents: " + contents + ".");
+
+            FileHandler.writeToFile(contents, senderID);
+
+            outputStream.flush();
+            outputStream.close();
+            inputStream.close();
+
+            return FileHandler.getFile(filename);
 
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
@@ -171,6 +259,11 @@ public class Main {
     private static boolean packetWasReceived(int currentIndex) {
         int packetToCheck = currentIndex - WINDOW_SIZE;
         return ackMap.get(packetToCheck).equals(true);
+    }
+
+    private static boolean shouldDropPacket() {
+        Random random = new Random();
+        return random.nextInt(100) < 1;
     }
 
     private static void updateAckMap(int index) {
--- csc445/Assignment2/Server.java
+++ csc445/Assignment2/Server.java
@@ -3,11 +3,17 @@ package Assignment2;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class Server {
+
+    static int WINDOW_SIZE = 4;
+    static Map<Integer, Boolean> ackMap = new HashMap<>();  // map to keep track of what packets have been acknowledged
+
     public static void main(String[] args) throws IOException {
         int port = 3030;
-        int WINDOW_SIZE = 4;
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             System.out.println("Listening on port " + port + "\n");
@@ -49,6 +55,7 @@ public class Server {
 
                             // send ack
                             outputStream.writeObject(packetToAck);
+                            outputStream.flush();
                             System.out.println("Ack sent for packet " + packetToAck);
 
                             packetToAck++;
@@ -74,11 +81,83 @@ public class Server {
                     } else if (mode == 1) {
                         System.out.println("Mode " + mode + "(Client is downloading a file)");
 
-                        String filename = (String) inputStream.readObject();
-                        File file = FileHandler.getFile(filename);
+                        long randomNumber = getRandomLong();
+                        long key = XorShift(randomNumber);
+                        XorEncryption xorEncryption = new XorEncryption(key);
 
-                        outputStream.writeObject(file);
+                        String filename = (String) inputStream.readObject();    // read filename
+                        File file = FileHandler.getFile(filename);  // get the file
+                        assert file != null;
+
+                        String contents = FileHandler.readFile(file);   // get the file contents
+                        assert contents != null;
+                        byte[] data = BytesHandler.stringToBytes(contents); // convert String -> bytes
+                        byte[] message = xorEncryption.encrypt(data, key);  // encrypt
+
+                        // fill ack map with every packet not yet acknowledged
+                        for (int i = 0; i < message.length; i++) {
+                            ackMap.put(i, false);
+                        }
+
+                        int senderIDLength = file.getName().length();
+                        int endIndex = senderIDLength - 4; // subtract off the extension
+                        String senderID = file.getName().substring(0, endIndex);   // get the files name without extension (senderID)
+
+                        outputStream.writeObject(senderID); // send the senderID
+                        outputStream.writeInt(message.length);   // send the message length
+                        outputStream.writeLong(randomNumber);    // send the random long for decryption
+
+                        // initialize variables
+                        int totalPackets = message.length;
+                        System.out.println("Total packets: " + totalPackets);
+                        int ackReceived = 0;
+                        int startPacket = -1;
+
+                        // sending file
+                        do {
+                            if (startPacket == -1) {
+                                for (int i = 0; i < WINDOW_SIZE; i++) {
+                                    byte packet = message[i];
+                                    outputStream.writeObject(packet);
+                                    outputStream.flush();
+                                    System.out.println("Sent packet " + i);
+                                }
+                            }
+
+                            // send the rest upon receiving expecting ack of packetToSend - WINDOW_SIZE
+                            try {
+                                Object ack = inputStream.readObject();
+
+                                if (ack instanceof Integer) {
+                                    ackReceived = (Integer) ack;
+                                    updateAckMap(ackReceived);
+                                }
+
+                                int remainingPackets = message.length - ackReceived;
+
+                                if (remainingPackets != 0) {
+                                    if (remainingPackets > WINDOW_SIZE) {
+                                        startPacket = ackReceived + WINDOW_SIZE;  // slide window
+                                        System.out.println("Window slid, sending packet " + startPacket);
+                                        if (startPacket < message.length && packetWasReceived(startPacket)) {
+                                            byte packet = message[startPacket];
+                                            outputStream.writeObject(packet);
+                                        }
+                                    }
+                                }
+
+                            } catch (ClassNotFoundException e) {
+                                e.printStackTrace();
+                            }
+
+                        } while (ackReceived != message.length);
+
+                        // flush and close streams
                         outputStream.flush();
+                        outputStream.close();
+                        inputStream.close();
+
+                        System.out.println("All packets acknowledged. (" + ackReceived + " packets)");
                     }
 
                 } catch (Exception e) {
@@ -88,10 +167,25 @@ public class Server {
         }
     }
 
+    private static void updateAckMap(int index) {
+        ackMap.replace(index, true);
+    }
+
+    private static boolean packetWasReceived(int currentIndex) {
+        int packetToCheck = currentIndex - WINDOW_SIZE;
+        return ackMap.get(packetToCheck).equals(true);
+    }
+
+
     private static long XorShift(long r) {
         r ^= r << 13;
         r ^= r >>> 7;
         r ^= r << 17;
         return r;
+    }
+
+    private static long getRandomLong() {
+        Random random = new Random();
+        return random.nextLong();
     }
 }
